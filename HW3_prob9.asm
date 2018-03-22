d18461b919054cc1eed4599156fe8984fef50c02
epartid1 <chequezkiel08@gmail.com>
2018-03-22 12:14:06 -0700

    Update HW3_prob9.asm

--- HW3_prob9.asm
+++ HW3_prob9.asm
@@ -4,7 +4,7 @@
 _start:     mov         rax, 1
             mov         rdi, 1
             mov         rsi, message
-            mov         rdx, 19
+            mov         rdx, 17
             syscall
             mov         rax, 60
             xor         rdi, rdi
